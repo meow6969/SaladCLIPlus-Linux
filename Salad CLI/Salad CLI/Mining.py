da0f0fc57ceee70cf3dc0c5ba0494a05d5852528
Walkx <71191962+Walker21390@users.noreply.github.com>
2021-04-10 13:06:33 +0200

    Update Mining.py

--- Salad CLI/Salad CLI/Mining.py
+++ Salad CLI/Salad CLI/Mining.py
@@ -14,13 +14,13 @@ refresh_time = 15  # seconds
 color = '0A'  # like u would type "color 0A" into cmd / leave empty for default
 
 os.system('color ' + color)
-select = input("Select miner! \n1. Phoenixminer 5.4c (GPU) \n2. XMRig 6.7.0 (CPU) \n3. XMRig-nVidia (GPU) \n4. XMRig-Amd (GPU \n5. Return to main menu \nSelect: ")
+select = input("Select miner! \n1. Phoenixminer 5.5c (GPU) \n2. XMRig 6.7.0 (CPU) \n3. XMRig-nVidia (GPU) \n4. XMRig-Amd (GPU \n5. Return to main menu \nSelect: ")
 if select == "1" or select == "Phoenixminer":
 	os.system("OptimiseGPU.bat")
-#	print("%appdata%\salad\plugin-bin\PhoenixMiner-5.4c\PhoenixMiner.exe -rmode 0 -rvram 1 -pool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -pool2 stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal "+ (phoenixwallet) + " -esm 3 -allpools 1 -allcoins 0")
+#	print("%appdata%\salad\plugin-bin\PhoenixMiner-5.5c\PhoenixMiner.exe -rmode 0 -rvram 1 -pool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -pool2 stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal "+ (phoenixwallet) + " -esm 3 -allpools 1 -allcoins 0")
 #	time.sleep(1100000)
 	os.system("mode 230,60")	
-	os.system(r"PhoenixMiner-5.4c\PhoenixMiner.exe -logfile phoenixlog.txt -rmode 0 -rvram 1 -pool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -pool2 stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal "+ (wallet) + " -esm 3 -allpools 1 -allcoins 0")
+	os.system(r"PhoenixMiner-5.5c\PhoenixMiner.exe -logfile phoenixlog.txt -rmode 0 -rvram 1 -pool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -pool2 stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -ewal "+ (wallet) + " -esm 3 -allpools 1 -allcoins 0")
 if select == "2" or select == "XMrig CPU":
 	os.system("cls")
 #	print(r"%appdata%\salad\plugin-bin\XMRig-CPU-6.7.0\xmrig.exe --donate-level=1 -o stratum+tcp://randomxmonero.usa.nicehash.com:3380 --coin=monero -u "+ (xmrwallet) + " -k --nicehash -o stratum+tcp://randomxmonero")
